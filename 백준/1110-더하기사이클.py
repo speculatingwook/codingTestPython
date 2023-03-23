dca6b25a72707df25c282b109e63654f23cfd984
Brian <bwook9908@gmail.com>
2023-03-23 10:00:31 +0900

    boj: 1110

---
+++ 백준/1110-더하기사이클.py
@@ -0,0 +1,21 @@
+def data_input():
+    N = int(input())
+    return N
+
+
+def solution():
+    N = data_input()
+    num = N
+    count = 0
+    while True:
+        a = num // 10
+        b = num % 10
+        c = (a + b) % 10
+        num = (b * 10) + c
+        count += 1
+        if (num == N):
+            break
+    return count
+
+
+print(solution())
